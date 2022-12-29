d1b00061e27f5ff0472d05ae8f712f5de20e8f6e
MZG0969 <muriloabreucastelobranco@gmail.com>
2022-12-29 17:26:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
     "MAIL_PORT": 587,
     "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": False,
+    "MAIL_USE_SSL": True,
     "MAIL_USERNAME": os.getenv("EMAIL"),
     "MAIL_PASSWORD": os.getenv("SENHA")
 }
