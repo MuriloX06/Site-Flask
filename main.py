679a9ad4377e83de672f049ed963308db9db014d
MZG0969 <muriloabreucastelobranco@gmail.com>
2022-12-30 23:24:44 -0300

    Update main.py - E-mail

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 from flask import Flask, render_template, redirect, request, flash
-from flask_mail import Mail, Message
+import smtplib
+import email.message
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'portfolio'
-
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 587,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.getenv("EMAIL"),
-    "MAIL_PASSWORD": os.getenv("SENHA")
-}
-
-app.config.update(mail_settings)
-mail = Mail(app)
 
 class Contato:
     def __init__(self, nome, email, mensagem):
@@ -32,25 +20,28 @@ def index():
 @app.route('/send', methods=['GET', 'POST'])
 def send():
     if request.method == 'POST':
-        formContato = Contato(
+        fm = Contato(
             request.form["nome"],
             request.form["email"],
             request.form["mensagem"]
         )
 
-        msg = Message(
-            subject = f'{formContato.nome} te enviou uma mensagem no portfólio',
-            sender = app.config.get("MAIL_USERNAME"),
-            recipients= [app.config.get("MAIL_USERNAME")],
-            body = f'''
-            
-            {formContato.nome} com o e-mail {formContato.email}, te enviou a seguinte mensagem:
+        corpo_email = f"""
+        <p>O {fm.nome} mandou uma mensagem: {fm.mensagem}</p>
+        """
 
-            {formContato.mensagem}
+        msg = email.message.Message()
+        msg['Subject'] = f"O {fm.nome} mandou mensagem do portfólio."
+        msg['From'] = os.getenv("EMAIL")
+        msg['To'] = 'muriloabreucastelobranco@gmail.com'
+        password = os.getenv("SENHA")
+        msg.add_header('Content-Type', 'text/html')
+        msg.set_payload(corpo_email)
 
-            '''
-        )
-        mail.send(msg)
+        s = smtplib.SMTP('smtp.gmail.com: 587')
+        s.starttls()
+        s.login(msg['From'], password)
+        s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
         flash('Mensagem enviada com sucesso!')
     return redirect('/')
 
